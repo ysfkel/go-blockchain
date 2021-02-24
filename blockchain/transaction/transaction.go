c772c8e8160f41291675ed0b7529b92e66178a75
ysfkel <yusufkel@gmail.com>
2021-02-24 17:21:51 +0400

    update

--- blockchain/transaction/transaction.go
+++ blockchain/transaction/transaction.go
@@ -1,12 +1,57 @@
 package transaction
 
+import (
+	"bytes"
+	"fmt"
+)
+
+type Transaction struct {
+	ID       []byte
+	Inputs   []TxInput
+	TxOutput []TxOutput
+}
+
 type TxOutput struct {
-	Value  int
-	PubKey string
+	Value  int    // value that is locked in the output / an output is indivisible
+	PubKey string // needed to unlock that tokens in the value field,
 }
 
 type TxInput struct {
 	ID  []byte
 	Out int
-	Sig string
+	Sig string // provdies data which is used to the outputs pub key
+}
+
+func (tx *Transaction) SetID() {
+	var encoded bytes.Buffer
+	var hash [32]byte
+	//encode :=
+}
+
+func CoinbaseTx(to, data string) *Transaction {
+
+	if data == "" {
+		data = fmt.Sprintf("cOINS TO %S", to)
+	}
+
+	txin := TxInput{
+		ID:  []byte{}, //coinbase tx references no output
+		Out: -1,       // -1 because coinbase tx references no output
+		Sig: data,
+	}
+
+	txout := TxOutput{
+		Value:  100,
+		PubKey: to, // if an account named jack mines this block, then jack gets these tokens
+	}
+
+	tx := Transaction{
+		nil,
+		[]TxInput{txin},
+		[]TxOutput{txout},
+	}
+	tx.SetID()
+
+	return &tx
+
 }
--- main.go
+++ main.go
@@ -1,19 +1,28 @@
 package main
 
 import (
-	"os"
-
-	blc "github.com/ysfkel/go-blockchain/blockchain"
-	db "github.com/ysfkel/go-blockchain/blockchain/db"
-	cmd "github.com/ysfkel/go-blockchain/cmd"
+	"fmt"
+	"math"
+	// "os"
+	// blc "github.com/ysfkel/go-blockchain/blockchain"
+	// db "github.com/ysfkel/go-blockchain/blockchain/db"
+	// cmd "github.com/ysfkel/go-blockchain/cmd"
 )
 
 func main() {
-	defer os.Exit(0)
-	chain := blc.InitBlockChain(db.NewRepository())
-	// close db after go channel exits properly (runtime.Goexit())
-	defer chain.Database.Close()
+	// defer os.Exit(0)
+	// chain := blc.InitBlockChain(db.NewRepository())
+	// // close db after go channel exits properly (runtime.Goexit())
+	// defer chain.Database.Close()
 
-	cli := cmd.CommandLine{chain}
-	cli.Run()
+	// cli := cmd.CommandLine{chain}
+	// cli.Run()
+	var x uint = 0
+	var y uint = 1
+	z := x - y
+	fmt.Println(z)
+
+	var r float64 = math.Pow(2, 256)
+	fmt.Println(r)
+	fmt.Println(r + 7)
 }
